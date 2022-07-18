ce499effcc7d6cdd698d37c0b9e8c1c0401ef950
macwray17 <55639501+macwray17@users.noreply.github.com>
2022-07-17 22:07:11 -0600

    Added regression

--- CAPM.py
+++ CAPM.py
@@ -10,7 +10,10 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-
+#market interest rate
+RISK_FREE_RATE = 0.05
+#considering monthly returns, want annual returns
+MONTHS_IN_YEAR = 12
 class CAPM:
     
     def __init__(self, stocks, start_date, end_date):
@@ -48,11 +51,37 @@ class CAPM:
         #beta = cov(a, M)/var(M)
         covariance_matrix = np.cov(self.data['s_returns'], self.data['m_returns'])
         beta = covariance_matrix[0, 1]/covariance_matrix[1,1]
-        print("Calculated beta: ", beta)
+        print("Beta from formula: ", beta)
+    
+    
+    def regression(self):
+        #performs a linear regression betwween expected market returns and 
+        #expected stock returns
+        beta, alpha = np.polyfit(self.data['m_returns'], self.data['s_returns'], deg = 1)
+        print("Beta from regression: ", beta)
+        print("Alpha from regression: ", alpha)
+        
+        #determining expected return using the CAPM formula
+        expected_return = RISK_FREE_RATE + beta *(self.data['m_returns'].mean() * MONTHS_IN_YEAR
+                                                  - RISK_FREE_RATE)
+        print("Expected return: ", expected_return)
+        self.plot_regression(alpha, beta)
+        
+    def plot_regression(self, alpha, beta):
+        fig, axis = plt.subplots(1, figsize = (20, 10))
+        axis.scatter(self.data['m_returns'], self.data['s_returns'], label = 'Data Points')
+        axis.plot(self.data['m_returns'], beta * self.data['m_returns'] + alpha, color = 'red', label = "CAPM Line")
+        plt.title('Capital Asset Pricing Model')
+        plt.xlabel('Market return $R_m$', fontsize = 18)
+        plt.ylabel('Stock return $R_a$')        
+        plt.legend()
+        plt.grid(True)
+        plt.show()
         
 if __name__ == '__main__':
     capm = CAPM(['IBM', '^GSPC'], '2010-01-01', '2017-01-01')
     capm.initialize()
     capm.calculate_beta()
+    capm.regression()
     
         
